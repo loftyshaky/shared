8ea116ed02d723be8482364671edb8ef9e3f3127
loftyshaky <loftyshaky@gmail.com>
2020-11-27 09:35:36 +1000

    feat: add help styles and messageы

--- src/ts/inputs/components/help.tsx
+++ src/ts/inputs/components/help.tsx
@@ -6,6 +6,9 @@ import {
     o_inputs,
     i_inputs,
 } from 'inputs/internal';
+import {
+    u_settings,
+} from 'settings/internal';
 
 interface Props {
     section_or_input: o_inputs.Section | i_inputs.Input;
@@ -14,6 +17,8 @@ interface Props {
 export class Help extends React.Component<Props> {
     public render(): JSX.Element {
         const { section_or_input } = this.props;
+        const section_name: string | undefined = (section_or_input as i_inputs.Input).section;
+        const is_input: boolean = n(section_name);
 
         return (
             <BaseTr
@@ -21,6 +26,11 @@ export class Help extends React.Component<Props> {
                 name='fade'
                 cls='help'
                 state={section_or_input.help_is_visible as boolean}
+                style={{
+                    width: is_input
+                        ? u_settings.InputsWidth.i.width[section_name!]
+                        : '',
+                }}
             >
                 {ext.msg(`${section_or_input.name}_help_text`)}
             </BaseTr>
