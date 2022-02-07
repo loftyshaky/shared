2a2fb780e75d51dc4c46b4b78177fc6f0cb658f9
loftyshaky <loftyshaky@gmail.com>
2022-02-07 23:06:25 +1000

    feat: include browser in extension version

--- src/ts/shared/ext_version/components/body.tsx
+++ src/ts/shared/ext_version/components/body.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { observer } from 'mobx-react';
 
 export const Body: React.FunctionComponent = observer(() => (
-    <span className='ext_version'>{`v${ext.get_ext_version()}`}</span>
+    <span className='ext_version'>{`v${ext.get_ext_version()} ${env.browser}`}</span>
 ));
