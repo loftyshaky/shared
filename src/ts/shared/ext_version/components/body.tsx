f364d50397693da5eaf112062387ff3559cb46f6
loftyshaky <loftyshaky@gmail.com>
2022-02-10 08:07:43 +1000

    fix: env existence error

--- src/ts/shared/ext_version/components/body.tsx
+++ src/ts/shared/ext_version/components/body.tsx
@@ -2,5 +2,7 @@ import React from 'react';
 import { observer } from 'mobx-react';
 
 export const Body: React.FunctionComponent = observer(() => (
-    <span className='ext_version'>{`v${ext.get_ext_version()} ${env.browser}`}</span>
+    <span className='ext_version'>{`v${ext.get_ext_version()}${
+        Object.prototype.hasOwnProperty.call(window, 'env') ? ` ${env.browser}` : ''
+    }`}</span>
 ));
