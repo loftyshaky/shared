facee8e6a8ba6c2a88488f536aa0cb017fdb5d02
loftyshaky <loftyshaky@gmail.com>
2022-04-10 03:45:49 +1000

    feat:(inputs) enable Btn conditional hiding

--- src/ts/inputs/components/btn.tsx
+++ src/ts/inputs/components/btn.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 
-import { stop_propagation } from 'shared/internal';
+import { c_tr, stop_propagation } from 'shared/internal';
 import { p_inputs } from 'inputs/internal';
 
 export const Btn: React.FunctionComponent<p_inputs.Btn> = observer((props) => {
     const { input } = props;
 
     return (
-        <button
-            className={x.cls(['btn', 'text', 'inset_border', input.is_enabled_cls!(), input.name])}
-            type='button'
-            tabIndex={input.tab_index!()}
-            onClick={input.event_callback}
-            onKeyDown={stop_propagation}
+        <c_tr.BaseTr
+            tag='button'
+            name='fade'
+            cls={x.cls(['btn', 'text', 'inset_border', input.is_enabled_cls!(), input.name])}
+            attr={{
+                type: 'button',
+                tabIndex: input.tab_index!(),
+                onClick: input.event_callback,
+                onKeyDown: stop_propagation,
+            }}
+            state={
+                input.is_visible_computed!() &&
+                ((input.is_cut && data.settings.enable_cut_features) || !input.is_cut)
+            }
+            style={{ marginLeft: x.px(input.offset) }}
         >
             {ext.msg(`${input.name}_btn_text`) || input.alt_msg}
-        </button>
+        </c_tr.BaseTr>
     );
 });
