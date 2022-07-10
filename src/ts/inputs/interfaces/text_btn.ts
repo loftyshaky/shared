9b9eb91a9889fca700a15a18e1a73d071c13e666
loftyshaky <loftyshaky@gmail.com>
2022-07-10 14:56:48 +1000

    fix: ts error

--- src/ts/inputs/interfaces/text_btn.ts
+++ src/ts/inputs/interfaces/text_btn.ts
@@ -3,6 +3,6 @@ import { t } from 'shared/internal';
 export interface TextBtn {
     name: string;
     Svg: string;
-    visibility_cond?: t.CallbackVariadicVoid;
+    visibility_cond?: t.CallbackVariadicBoolean;
     event_callback: t.CallbackVariadicVoid;
 }
