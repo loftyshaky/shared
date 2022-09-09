2f5e4f525402ebd6b7d93fd707a6f1f7be0ab5dc
loftyshaky <loftyshaky@gmail.com>
2022-09-09 14:06:17 +1000

    feat(inputs) make link_btn hideable

--- src/ts/inputs/components/link_btn.tsx
+++ src/ts/inputs/components/link_btn.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 
+import { c_tr, stop_propagation } from 'shared/internal';
 import { p_inputs } from 'inputs/internal';
 
 export const LinkBtn: React.FunctionComponent<p_inputs.LinkBtn> = observer((props) => {
     const { input } = props;
 
     return (
-        <button
-            className={x.cls(['btn', 'link_btn', input.is_enabled_cls!(), input.name])}
-            type='button'
-            tabIndex={input.tab_index!()}
-            onClick={input.event_callback}
+        <c_tr.BaseTr
+            tag='button'
+            name='fade'
+            cls={x.cls(['btn', 'link_btn', input.is_enabled_cls!(), input.name])}
+            attr={{
+                type: 'button',
+                tabIndex: input.tab_index!(),
+                onClick: input.event_callback,
+                onKeyDown: stop_propagation,
+            }}
+            state={
+                input.is_visible_computed!() &&
+                ((input.is_cut && data.settings.enable_cut_features) || !input.is_cut)
+            }
+            style={{ marginLeft: x.px(input.offset) }}
         >
             {ext.msg(`${input.name}_link_btn_text`)}
-        </button>
+        </c_tr.BaseTr>
     );
 });
