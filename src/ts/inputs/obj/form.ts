d1c1052d83193d5391814f0e07f4b45379615f75
loftyshaky <loftyshaky@gmail.com>
2022-09-16 00:02:23 +1000

    fix(inputs): allow link type in form

--- src/ts/inputs/obj/form.ts
+++ src/ts/inputs/obj/form.ts
@@ -3,7 +3,7 @@ import { o_inputs, i_inputs } from 'inputs/internal';
 export class Form extends o_inputs.InputBase {
     public type?: 'form' = 'form';
     public action: string;
-    public inputs?: i_inputs.Inputs;
+    public inputs?: i_inputs.Inputs | i_inputs.Links;
 
     public constructor(obj: Form) {
         super(obj);
