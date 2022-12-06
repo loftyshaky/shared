f11b53bcd3962cb91cb82ad354e4bed8e4ae1537
loftyshaky <loftyshaky@gmail.com>
2022-12-07 02:37:48 +1000

    feat: add transpaerent_1px_png to vars

--- src/ts/shared/vars.ts
+++ src/ts/shared/vars.ts
@@ -1,1 +1,3 @@
 export const scrollbar_width = 17;
+export const transpaerent_1px_png =
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
