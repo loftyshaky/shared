b70aacc03e194291aaa6afecfb38a8adc1fe2cff
loftyshaky <loftyshaky@gmail.com>
2022-12-31 15:31:29 +1000

    refactor: rename things

--- src/ts/shared/app.ts
+++ src/ts/shared/app.ts
@@ -64,7 +64,7 @@ export class App {
     };
 
     public get_language = (): string =>
-        n(data) && n(data.settings) && data.settings.language ? data.settings.language : 'en';
+        n(data) && n(data.settings) && data.settings.locale ? data.settings.locale : 'en';
 
     private content_dir = (): string => {
         try {
