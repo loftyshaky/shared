a78fdec947ab7f1720973267b5269c15a22d12cd
loftyshaky <loftyshaky@gmail.com>
2023-08-16 23:46:35 +1000

    feat: allow excluding locales

--- js/shared/locales.js
+++ js/shared/locales.js
@@ -5,8 +5,10 @@ const fs = require('fs-extra');
 const { is_ext } = require('./apps');
 
 class Locales {
-    constructor({ app_root }) {
+    constructor({ app_root, exclude_shared_locales = [] }) {
+        // exclude_shared_locales: if an extension has an uncomplete messages.json for a locale (the extension itself, not shared), this setting prevents a messages.json form shared mixing with it. Note: If an extension (the extension itself, not shared) doesn't have a messages.json for a locale, you don't need to use this settings because this script is looking for locale forlders (const locales = fs.readdirSync(app_locales_path);) in an extension (the extension itself, not shared) and then loping through them based on locales found (locales.forEach((locale) => {)
         this.app_root = app_root;
+        this.exclude_shared_locales = exclude_shared_locales;
     }
 
     merge = async ({ env }) => {
@@ -25,6 +27,8 @@ class Locales {
         const locales = fs.readdirSync(app_locales_path);
 
         locales.forEach((locale) => {
+            const locale_is_excluded = this.exclude_shared_locales.includes(locale);
+
             const shared_messages_path = path.join(shared_locales_path, locale, 'messages.json');
 
             const app_messages = fs.readJSONSync(
@@ -39,10 +43,12 @@ class Locales {
                 );
             }
 
-            const merged_messages = {
-                ...shared_messages,
-                ...app_messages,
-            };
+            const merged_messages = locale_is_excluded
+                ? app_messages
+                : {
+                      ...shared_messages,
+                      ...app_messages,
+                  };
             const locale_path = path.join(
                 this.app_root,
                 env === 'adonis_app' ? path.join('public', 'assets') : 'dist',
