c31b3fb42991689f13e1e677cdf32561c4720cb0
loftyshaky <loftyshaky@gmail.com>
2024-08-09 23:57:13 +1000

    refactor: place some folders in correct place

--- src/ts/settings/data/index.ts
+++
@@ -1,2 +0,0 @@
-export * from 'settings/data/back_up';
-export * from 'settings/data/sections';
---
+++ src/ts/settings/settings/data/index.ts
@@ -0,0 +1,2 @@
+export * from 'settings/settings/data/back_up';
+export * from 'settings/settings/data/sections';
